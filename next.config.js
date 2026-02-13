ba9811f9685119e8236ed89a94ee4b2fa03ab708
Thean Lim <theanlim@amazon.com>
2026-02-12 20:53:22 -0800

    fix: use the correct base path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
-const basePath = isProd ? '/digital-date-night' : '';
+const basePath = isProd ? '/task-manager' : '';
 
 const nextConfig = {
   output: 'export',
